e6cf1f5a3d66259333616793a634e7e0dc911dc2
Sean <saryana0@uw.edu>
2014-04-26 03:50:22 -0700

    nothing

--- app.js
+++ app.js
@@ -3,21 +3,44 @@ var express = require('express'),
 	ntr	= require('nutritionix')({
 		appId: constants.APPID,
 		appKey: constants.APP_KEY
-	}, true),
+	}, true), // set true to help debug
 	path  = require('path'),
 	app = express();
 
+// set the port I <3 boobs
 app.set('port', 8008);
+// share the love, make public available
 app.use(express.static(path.join(__dirname, 'public')));
-
+// Read the body of the page
+app.use(express.bodyParser());
+// get the homepage using basic html
 app.get('/', function(req, res) {
 	res.sendfile('views/index.html', {title:'WTE'});
 });
 
+// get the post request and run query
 app.post('/', function(req, res) {
-
+	// Get the fields here
+	// restaurant
+	var restaurant = res.body.restaurant;
+	nutritionix.v1_1.search.advanced({
+	    fields: ['item_name','brand_name','item_description','nf_calories','nf_total_fat','nf_cholesterol','nf_sodium','nf_total_carbohydrate','nf_dietary_fiber','nf_protein'],
+	    queries: {
+	    	"brand_name": restaurant
+	    },
+	    offset:0,
+	    limit:1
+	}, function (err, results) {
+	    if (err) console.log(err);
+	    if (results.total) {
+	    	console.log(results);
+	    } else {
+	    	console.log('No results...');
+	    }
+	});
 });
 
+// start the server
 app.listen(app.get('port'), function () {
 	console.log('I love boobies so I run on port ' + app.get('port'));
 });
