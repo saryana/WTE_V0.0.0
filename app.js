687a1c13fe67ab839a1fa9ca7e5430eab426e484
Sean <saryana0@uw.edu>
2014-04-26 04:20:46 -0700

    steven is the worst

--- app.js
+++ app.js
@@ -19,17 +19,21 @@ app.get('/', function(req, res) {
 });
 
 // get the post request and run query
-app.post('/', function(req, res) {
+app.post('/data', function(req, res) {
 	// Get the fields here
 	// restaurant
-	var restaurant = res.body.restaurant;
+	console.log(req.body());
+	/*var restaurant = res.body.restaurant;
 	nutritionix.v1_1.search.advanced({
 	    fields: ['item_name','brand_name','item_description','nf_calories','nf_total_fat','nf_cholesterol','nf_sodium','nf_total_carbohydrate','nf_dietary_fiber','nf_protein'],
 	    queries: {
-	    	"brand_name": restaurant
+	    	brand_name: restaurant
 	    },
 	    offset:0,
-	    limit:1
+	    limit:20,
+	    filters: {
+	    	item_type: 1
+	    },
 	}, function (err, results) {
 	    if (err) console.log(err);
 	    if (results.total) {
@@ -37,7 +41,7 @@ app.post('/', function(req, res) {
 	    } else {
 	    	console.log('No results...');
 	    }
-	});
+	});*/
 });
 
 // start the server
